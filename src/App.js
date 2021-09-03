f3e25b03bab9f356e247df422483912bd2b027ff
Adrian <leongadrian36@gmail.com>
2021-09-03 12:47:14 +0800

    initialize sample components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
+import Page3 from "./components/Page3";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [route, setRoute] = useState("page1");
+
+	const onRouteChange = (route) => setRoute(route);
+
+	return (
+		<div className="App">
+			{route === "page1" ? <Page1 onRouteChange={onRouteChange} /> : null}
+			{route === "page2" ? <Page2 onRouteChange={onRouteChange} /> : null}
+			{route === "page3" ? <Page3 onRouteChange={onRouteChange} /> : null}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Page3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page1 = ({ onRouteChange }) => (
+	<div className="App">
+		<button onClick={() => onRouteChange("page1")}>Page 1</button>
+		<button onClick={() => onRouteChange("page2")}>Page 2</button>
+		<button className="disabled">Page 3</button>
+	</div>
+);
+
+export default Page1;
